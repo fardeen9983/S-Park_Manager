823d0504af2d245981d2d4d50e66443546048581
Fardeen Khan <khanfardeen9983@gmail.com>
2018-03-24 15:25:26 +0530

    qr code scanner added

--- app/src/main/java/android/example/com/sparkmanager/DashboardActivity.java
+++ app/src/main/java/android/example/com/sparkmanager/DashboardActivity.java
@@ -1,24 +1,54 @@
 package android.example.com.sparkmanager;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class DashboardActivity extends AppCompatActivity {
     private Button qrScan;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-
+        final Activity activity = DashboardActivity.this;
         qrScan = findViewById(R.id.qr_scan);
         qrScan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),QRScannerActivity.class));
+
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.setPrompt("Scan");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(true);
+                integrator.setBarcodeImageEnabled(true);
+                integrator.initiateScan();
             }
         });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "You cancelled this Scanning", Toast.LENGTH_LONG).show();
+            } else {
+                Intent intent = new Intent(getApplicationContext(),QRScannerActivity.class);
+                intent.putExtra("qr",result.getContents());
+                startActivity(intent);
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
--- app/src/main/java/android/example/com/sparkmanager/QRScannerActivity.java
+++ app/src/main/java/android/example/com/sparkmanager/QRScannerActivity.java
@@ -1,13 +1,36 @@
 package android.example.com.sparkmanager;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class QRScannerActivity extends AppCompatActivity {
-
+    private TextView area_text;
+    private String qrCode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrscanner);
+        area_text = (TextView) findViewById(R.id.qrcode);
+
+        Intent intent = getIntent();
+        qrCode = intent.getStringExtra("qr");
+        if(qrCode!=null){
+            area_text.setText(qrCode);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(getApplicationContext(),DashboardActivity.class));
     }
 }
